20e401923b5b02e4449ed408a146775db3ef2f79
davelens <github@davelens.be>
2025-06-30 12:42:52 +0200

    Bump to 0.2.1.

--- lib/active_collab.rb
+++ lib/active_collab.rb
@@ -4,7 +4,7 @@ require 'uri'
 require 'active_support/all'
 
 module ActiveCollab
-  VERSION = '0.2.0'
+  VERSION = '0.2.1'
 
   require_relative 'active_collab/client.rb'
   require_relative 'active_collab/response.rb'
